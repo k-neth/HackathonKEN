8d28e94f731d20c4cb149a8874a4c92f40391301
k-neth <kneth46@gmail.com>
2024-08-20 01:02:44 +0300

    revised

--- problemsolving.py
+++ problemsolving.py
@@ -1,12 +1,30 @@
 
-enternum = input("Enter a list of integers separated by spaces: ")
+def entryPrompt():
+    enternum = input("Enter a list of integers separated by spaces: ")
+    return enternum
+    
+def myFunction(enternum):
+    try:
+        num_list = list(map(int, enternum.split()))
+    
+        uniquenums = list(set(num_list))
 
-num_list = list(map(int, enternum.split()))
+        uniquenums.sort(reverse=True)
 
-uniquenums = list(set(num_list))
+        print(uniquenums)
+
+    except ValueError:
+        print( "invalid input")
+        
+
+def myOutput():
+    while True:
+        enternum=entryPrompt()
+        myFunction(enternum)
+    
+myOutput()
 
 
-uniquenums.sort(reverse=True)
+   
+   
 
-
-print("nums with no duplicates:", uniquenums)
